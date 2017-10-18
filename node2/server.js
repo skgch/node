876b7fea91e8cda14754754839a955f4b6ae1a8b
skgch <m.sakaguchi04@gmail.com>
2017-10-19 00:57:22 +0900

    Add BBS

--- node2/server.js
+++ node2/server.js
@@ -1,22 +1,39 @@
 var http = require('http');
 var fs = require('fs');
 var ejs = require('ejs');
+var qs = require('querystring');
 var settings = require('./settings');
 
 var server = http.createServer();
 
-var template = fs.readFileSync(__dirname + '/public/hello.ejs', 'utf-8');
-var n = 0;
-server.on('request', function(req, res){
-    n++;
-    data = ejs.render(template, {
-        title: 'hello',
-        content: '<strong>World</strong>',
-        n: n
+var template = fs.readFileSync(__dirname + '/public/bbs.ejs', 'utf-8');
+var posts = [];
+
+function renderForm(posts, res) {
+    var data = ejs.render(template, {
+        posts: posts
     });
     res.writeHead(200, {'ContentType' : 'text/html'});
     res.write(data);
     res.end();
+}
+
+server.on('request', function(req, res){
+    if(req.url !== '/favicon.ico') {
+        if(req.method==="POST"){
+            req.data = "";
+            req.on('readable', function(){
+                req.data += req.read();
+            });
+            req.on('end', function() {
+                var query = qs.parse(req.data);
+                posts.push(query.name);
+                renderForm(posts, res);
+            });
+        } else {
+            renderForm(posts, res);
+        }    
+    }
 });
 
 server.listen(settings.port, settings.host);
